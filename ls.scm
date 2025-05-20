f492bf69644c6face58e657f6a4a98f4902fbe31
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2025-05-20 21:35:34 +0900

    add ls

---
+++ ls.scm
@@ -0,0 +1,24 @@
+(define first-non-option
+  (lambda argv
+    (if (null? argv)
+      '()
+      (let ((a (car argv)))
+        (if (and (string? a) (= #\- (string-ref a 0)))
+          (first-non-option (cdr argv))
+          a)))))
+
+(define orelse
+  (lambda argv
+    (if (null? argv)
+      '()
+      (let ((a (car argv)))
+        (if (null? a)
+          (apply orelse (cdr argv))
+          (car a))))))
+
+(define ls
+  (lambda argv
+    (let ((name (orelse (first-non-option argv) ".")))
+      (if (file-directory? name)
+        (directory-list name)
+        (if (file-exists? name) name '())))))
